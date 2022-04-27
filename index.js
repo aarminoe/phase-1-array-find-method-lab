5a073d64a9e21c003acb77f882d49c48122aa4fa
Aaron Noe <aarminoe@gmail.com>
2022-04-27 15:04:26 -0500

    commit tree 0e7eebe71121ac174f14d1e0b026db0dcb9584df

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+/*
+function superbowlWin(array) {
+    const foundW = array.find(isW)
+    if (foundW.result === 'W') {
+        return foundW.year
+    }
+    else {
+        return undefined
+    }
+}
+
+function isW(element) {
+    return element.result === 'W'
+}
+
+
+
+/*
+//Works
+function superbowlWin(array) {
+    for (const win of array) {
+        if (win.result === 'W') {
+            return win.year
+        }
+    }
+}
+*/
+
+
+function superbowlWin(array) {
+    let obj = array.find(isW)
+    return obj.year
+}
+
+function isW(element) {
+   return element.result === 'W'
+}
